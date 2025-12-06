6b94c9c289ef19484728313c4887d56adae93c57
kishanghosh090 <kishanghosh090@gmail.com>
2025-12-06 22:12:03 +0530

    add box decoration

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -8,17 +8,36 @@ void main() {
     ),
   );
 }
-// isActive
 
+// isActive
 class MyApp extends StatelessWidget {
   const MyApp({super.key, required this.title});
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text(" hello from chai", style: TextStyle())),
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+        // appBar: AppBar(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: AlignmentGeometry.topCenter,
+              colors: [
+                Colors.teal,
+                Colors.transparent,
+                Colors.redAccent,
+                Colors.pinkAccent,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(" hello from chai", style: TextStyle()),
+          ),
+        ),
+      ),
     );
   }
 }
